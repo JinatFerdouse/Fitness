5145a675034dcd7541fab2e87e002d163e219239
JinatFerdouse <58516299+JinatFerdouse@users.noreply.github.com>
2019-12-11 17:52:00 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+jQuery('.fadeinleft').addClass("hidden").viewportChecker({
+    classToAdd: 'visible animated fadeInLeft',
+    offset: 100
+   });
